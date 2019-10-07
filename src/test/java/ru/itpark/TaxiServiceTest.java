845458ec7a46a880005da7d0c90f4cb8e25fe350
Руфат_Ольга <rufattabaev@gmail.com>
2019-10-07 09:56:44 +0300

    fixes #5

--- src/test/java/ru/itpark/TaxiServiceTest.java
+++ src/test/java/ru/itpark/TaxiServiceTest.java
@@ -8,24 +8,27 @@ import static org.junit.jupiter.api.Assertions.*;
 class TaxiServiceTest {
 
     @Test
-    @DisplayName("Should calculate total cost of trip")
-    void count() {
-        {
-            TaxiService service = new TaxiService();
-            int totalCost = service.count(60, 20, 10);
-            assertEquals(260, totalCost);
-        }
-        {
-            TaxiService service = new TaxiService();
-            int totalCost = service.count(60, 20, 50);
-            assertEquals(1007, totalCost);
-        }
-        {
-            TaxiService service = new TaxiService();
-            int totalCost = service.count(60, 20, 100);
-            assertEquals(1960, totalCost);
-        }
+    @DisplayName("Should calculate total cost of trip when discount is not valid")
+    void countWithoutDiscount() {
+        TaxiService service = new TaxiService();
+        int totalCost = service.count(60, 20, 10);
+        assertEquals(260, totalCost);
     }
 
 
+    @Test
+    @DisplayName("Should calculate total cost of trip when discount is valid and not exceed discount limit")
+    void countWithDiscount() {
+        TaxiService service = new TaxiService();
+        int totalCost = service.count(60, 20, 50);
+        assertEquals(1007, totalCost);
+    }
+
+    @Test
+    @DisplayName("SHould calcelate total cost of trip when discount is valid and exceeds discount limit")
+    void countWithMaxDoscount() {
+        TaxiService service = new TaxiService();
+        int totalCost = service.count(60, 20, 100);
+        assertEquals(1960, totalCost);
+    }
 }
