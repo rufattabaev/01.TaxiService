de2a0fab47f74279b5da084f85afee5cbce4bbec
Руфат_Ольга <rufattabaev@gmail.com>
2019-10-07 09:39:26 +0300

    fixes #6

--- src/main/java/ru/itpark/TaxiService.java
+++ src/main/java/ru/itpark/TaxiService.java
@@ -3,10 +3,11 @@ package ru.itpark;
 public class TaxiService {
     private int startOfDiscount = 1000;
     private int discountLimit = 100;
+    double discountRate = 0.95;
 
     int count(int boardingCost, int kilometerCost, int distance) {
         int totalCost = boardingCost + (kilometerCost * distance);
-        int discount = (int) (totalCost - (totalCost * 0.95));
+        int discount = (int) (totalCost - (totalCost * discountRate));
 
         if (totalCost < startOfDiscount) {
             return totalCost;
